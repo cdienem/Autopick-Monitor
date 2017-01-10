b6bb57a72a53b9f0bbe45aee9d652c3e2cbf7a53
cdienem <cdienem@users.noreply.github.com>
2017-01-10 18:31:12 +0100

    some changes for more output

--- monitor.sh
+++ monitor.sh
@@ -6,11 +6,13 @@
 
 while [ 1 ];
 do
-	printf "\033c"
-	number=$(more *auto* | grep . | wc -l)	
-	files=$(ls *autopick* | wc -l)
-	avg=$(expr $number / $files)
-	echo "Processed micrographs: $files"	
-	echo "Picked particles: $number ($avg per mic)";
-	sleep 5;
+  	printf "\033c"
+        number=$(more *auto* | grep . | wc -l)
+        all=$(ls *.mrc | wc -l)
+        files=$(ls *autopick* | wc -l)
+        avg=$(expr $number / $files)
+        pred=$(expr $all \* $avg)
+        echo "Processed micrographs: $files"
+        echo "Picked particles: $number (Avg: $avg per mic; Pred: $pred)";
+        sleep 60;
 done;
